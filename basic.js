9f851577ac27942ec8733183a8401c11765d4d07
mdnh42 <mdnh.42@gmail.com>
2021-08-23 22:13:36 +0600

    Module practise

---
+++ basic.js
@@ -0,0 +1,58 @@
+//Video - 1;
+
+// Variable Declare
+var deposit = 400; 
+// condition 
+if(deposit > 500){
+    
+}
+else if(deposit < 200){
+
+}
+
+else if (deposit == 500){
+
+}
+
+else if(deposit != 200){
+
+}
+
+else if(deposit >= 500){
+    
+}
+
+else if(deposit <= 500){
+
+}
+else{
+
+}
+
+// array
+const numbers = [34, 23, 2, 234, 213, 53];
+// console.log(number.length);
+// console.log(number.pop());
+// console.log(number.push(3434));
+
+// console.log(number[2] = 333);
+// if(number.indexOf(222) != -1){
+
+// }
+
+// loop
+// while , for
+/* for(const number of numbers){
+    console.log(number);
+}
+ */
+function funllName (first, second){
+    const name = first + ' ' + second;
+    return name;
+}
+
+const person = funllName('Rahim', 'Ali');
+console.log(person);
+
+//object
+const bottle = {color: 'yellow', contain: 'Water', price = '50', }
---
+++ variable.js
@@ -0,0 +1,35 @@
+/* const balance = 1200;
+balance - 1250;
+console.log(balance);
+const userName = 'Janpaki Potas Potas';
+console.log(userName);
+const weTogather = 'ami' + userName;
+console.log(weTogather);
+
+userName = 'Moyna Pakhi'; */
+
+const numbers = [2, 1, 2, 5];
+// numbers = [99, 32, 42 ,24]; // not allowed
+numbers.push(423);
+
+console.log(numbers);
+numbers.pop();
+numbers[1] = 3;
+console.log(numbers);
+let sum = 0;
+for(let i = 0; i <numbers.length; i++){
+    const numebr = numbers[i];
+    console.log(numebr);
+    sum = sum + numebr;
+    
+}
+console.log(sum);
+
+
+/* const student = {roll :1, name : 'Mofiz', job : 'intern' };
+student.name = 'Mofazzol';
+console.log(student);
+student = 'Nur';
+// console.log(student);
+ */
+
