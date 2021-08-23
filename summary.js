ff95ba250cbe4ca17565707aef1e4d0f00a46c36
mdnh42 <mdnh.42@gmail.com>
2021-08-24 00:50:14 +0600

    Summary

---
+++ summary.js
@@ -0,0 +1,44 @@
+let Variable 
+// Variables defined with let cannot be Redeclared.
+// Variables defined with let must be Declared before use.
+// Variables defined with let have Block Scope.
+
+functon 
+// Parameter handling 
+// Default Parameter
+
+
+// ES6 Function 
+
+
+// A JavaScript function is a block of code designed to perform a particular task.
+
+// A JavaScript function is executed when "something" invokes it (calls it).
+
+/* 
+A JavaScript function is defined with the function keyword, followed by a name, followed by parentheses ().
+
+Function names can contain letters, digits, underscores, and dollar signs (same rules as variables).
+
+The parentheses may include parameter names separated by commas:
+(parameter1, parameter2, ...)
+
+The code to be executed, by the function, is placed inside curly brackets: {}
+
+Function parameters are listed inside the parentheses () in the function definition.
+
+Function arguments are the values received by the function when it is invoked.
+
+Inside the function, the arguments (the parameters) behave as local variables. */
+
+/* The const keyword allows you to declare a constant (a JavaScript variable with a constant value).
+
+Constants are similar to let variables, except that the value cannot be changed. */
+
+
+`Template String `
+
+`${result}`;
+
+// with para ()
+// with one para not bracate
